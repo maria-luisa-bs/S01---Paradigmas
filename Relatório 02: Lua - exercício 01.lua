e100ff0e22d3c9f518ef2a43565cc1fc4e5d4da3
Maria Luisa Barroso <marialuisabarroso1908@hotmail.com>
2025-08-27 15:28:49 -0300

    Update Relatório 02: Lua - exercício 01.lua

--- Relatório 02: Lua - exercício 01.lua
+++ Relatório 02: Lua - exercício 01.lua
@@ -1,1 +1,20 @@
+print("Digite o valor de M: ")
+local M = tonumber(io.read())
 
+print("Digite o valor de N: ")
+local N = tonumber(io.read())
+
+print("Digite o valor de X: ")
+local X = tonumber(io.read())
+
+print("Múltiplos de " .. X .. " no intervalo de [" .. M .. ", " .. N .. "]:")
+
+local contador = 0
+for i = M, N do
+    if i % X == 0 then
+        print(i)
+        contador = contador + 1
+    end
+end
+
+print("Quantidade de múltiplos encontrados no intervalo: " .. contador)
