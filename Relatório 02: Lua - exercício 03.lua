1845d489a3839de1840e65e6f2d8387b6e491076
Maria Luisa Barroso <marialuisabarroso1908@hotmail.com>
2025-08-28 19:51:12 -0300

    Update Relatório 02: Lua - exercício 03.lua

--- Relatório 02: Lua - exercício 03.lua
+++ Relatório 02: Lua - exercício 03.lua
@@ -1,27 +1,23 @@
-function filtropares(tabela)
-    local pares = {}
-    for i = 1, #tabela do
-        if tabela[i] % 2 == 0 then
-            table.insert(pares, tabela[i])
-        end
-    end
-    return pares
-end
-
-print("Digite a quantidade de números que contém na tabela:")
+local vetortabela = {}
+print("Digite a quantidade de números que deseja inserir na tabela: ")
 local n = tonumber(io.read())
+local novatabela = {}
+local quantidadepares = 0
 
-local numeros = {}
-
-print("Digite os números que serão inseridos da tabela:")
+print("Digite os numeros da tabela: ")
 for i = 1, n do
-    numeros[i] = tonumber(io.read())
+    vetortabela[i] = tonumber(io.read())
+   if vetortabela[i]%2 == 0 then
+      table.insert(novatabela, vetortabela[i])
+      quantidadepares = quantidadepares + 1
+  end
 end
 
-local parestabela = filtropares(numeros)
-
-
-print("Números da nova tabela:")
-for i = 1, #parestabela do
-    io.write(parestabela[i] .. " ")
+if quantidadepares == 0 then
+  print("Não existe números pares na tabela")
+else
+  print("Os números pares encontrados na tabela são: ")
+for i = 1, quantidadepares do
+  print(novatabela[i])
+end
 end
