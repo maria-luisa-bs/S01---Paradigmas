a62ac75160070ef404bce9d885678518a58f9e8e
Maria Luisa Barroso <marialuisabarroso1908@hotmail.com>
2025-11-28 10:29:28 -0300

    Create Relatório 14: Prolog - exercício 02.pl

---
+++ Relatório 14: Prolog - exercício 02.pl
@@ -0,0 +1,10 @@
+deus_maior(Deus) :-
+    local_principal(Deus, olimpo),    
+    dominio(Deus, Dom1),              
+    dominio(Deus, Dom2),  
+    
+    Dom1 \= Dom2.                     
+
+Deus = Zeus
+Deus = Apolo
+Deus = Atena
