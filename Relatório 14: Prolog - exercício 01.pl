9e8ff8e64d54b81b66fe4d0aed2f0f1c070b3b29
Maria Luisa Barroso <marialuisabarroso1908@hotmail.com>
2025-11-28 10:18:34 -0300

    Create Relatório 14: Prolog - exercício 01.pl

---
+++ Relatório 14: Prolog - exercício 01.pl
@@ -0,0 +1,13 @@
+divindade_olimpica(Deus) :-
+    progenitor(cronos, Deus),           
+    (
+        dominio(Deus, ceu);             
+        dominio(Deus, mar);             
+        dominio(Deus, submundo)         
+    ).
+
+ divindade_olimpica(X).
+
+X = zeus ;
+X = poseidon ;
+X = hades.
